48641b2628784484154afe1a92bf466dd93e8287
urbanodev81 <ricardo@focusmedia.com.br>
2024-06-14 18:38:32 -0300

    colocado imagem e area de touch para trocar imagem - aula Criando áreas sensíveis ao toque

--- App.tsx
+++ App.tsx
@@ -1,20 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+
+import symbolOn from './assets/pictures/symbol-on.png';
+import symbolOff from './assets/pictures/symbol-off.png';
+import { useState } from 'react';
 
 export default function App() {
+  const [isActive, setisActive] = useState(false)
+
+  function handleSymbol(){
+    
+    setisActive((oldValue:boolean) =>{
+      return !oldValue
+    })
+
+    console.log(isActive)
+  }
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app teste! ddd</Text>
+    <View style={isActive ? styles.containerOn : styles.containerOff}>
+      <TouchableOpacity onPress={handleSymbol} >
+              <Image source={isActive ? symbolOn : symbolOff} />
+      </TouchableOpacity>
+
+      <Text style={{color:'red'}}>Open up App.tsx to start working on your app teste! ddd</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  containerOn: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  containerOff:{
+    flex: 1,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
